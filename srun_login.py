ad717796f47d1701ac7c61d656c1378e7a1790b2
nuanxinqing123 <nuanxinqing@gmail.com>
2021-07-08 17:49:23 +0800

    修复# selenium和谷歌浏览器驱动引起的非致命错误

--- srun_login.py
+++ srun_login.py
@@ -11,16 +11,19 @@ def Start():
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-gpu')
+    options.add_argument('--disable-software-rasterizer')
 
     browser = webdriver.Chrome(options=options)
 
-    # 必须使用HTTP，使用HTTPS将无法跳转
+    # 一般情况：必须使用HTTP，使用HTTPS将无法跳转。否则反转
+    # 修改为自己学校的认证地址或者认证支持跳转的话，修改为http://www.baidu.com也行
     browser.get("http://172.16.0.88/srun_portal_pc?ac_id=1&theme=sxgy")
 
     try:
         # 输入自己的用户名,密码
         username_ = 'xxxyjyl'
         password_ = 'xinxixinxi'
+
         # 驱动输入用户名,密码
         username = browser.find_element_by_xpath('//*[@id="username"]')
         password = browser.find_element_by_xpath('//*[@id="password"]')
@@ -42,10 +45,17 @@ def Start():
             return "需要认证，已认证成功"
         finally:
             browser.quit()
-    except selenium.common.exceptions.NoSuchElementException as e:
+
+    except selenium.common.exceptions.NoSuchElementException:
         # print("认证已完成，无需再次认证")
         browser.quit()
         return "认证已完成，无需再次认证"
+
+    except selenium.common.exceptions.StaleElementReferenceException:
+        # 这个错误一般是因为已经认证完成而导致的页面跳转发生的无法找到对应元素
+        browser.quit()
+        return "认证已完成，无需再次认证"
+
     # 无论如何都要执行的任务
     finally:
         browser.quit()
